4aae38b5eb289c16e1cc97efcc6f547b0672d114
Akshith Kandivanam <akshithkandivanam@gmail.com>
2023-06-27 22:34:27 -0400

    Retrieval of training dataset
    Going to be retrieving more data soon, just a test run

--- data/data_download_script.py
+++ data/data_download_script.py
@@ -1,5 +1,16 @@
 # USING VOXEL51 TO DOWNLOAD DATASET FROM GOOGLE OPEN IMAGES DATASET (V7):
 
 # importing libraries
-import fiftyone as fo
+import cv2
+import fiftyone as f
+import glob
 import os
+
+# boilerplate code for dataset download:
+dataset = f.zoo.load_zoo_dataset(
+        "open-images-v7",
+        split="train", # split of dataset
+        label_types=["detections"], # type of labels
+        classes = ["Plastic bag"], # class of labels
+        max_samples=1000, # max number of samples to download
+)
