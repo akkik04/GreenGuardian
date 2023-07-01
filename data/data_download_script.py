3c3152932f7604f574454b746e5862cbda0591ca
Akshith Kandivanam <akshithkandivanam@gmail.com>
2023-07-01 19:05:49 -0400

    Modified the data retrieval script

--- data/data_download_script.py
+++ data/data_download_script.py
@@ -6,14 +6,18 @@ import fiftyone as f
 import glob
 import os
 
-splits = ["train", "validation", "test"] # splits of dataset
+splits = ["train", "validation"] # splits of dataset
 
 for s in splits:
+        if(s == "validation"):
+                samples = 10
+        else:
+                samples = 1000
         # boilerplate code for dataset download:
         dataset = f.zoo.load_zoo_dataset(
                 "open-images-v7",
                 split = s, # split of dataset
                 label_types=["detections"], # type of labels
                 classes = ["Plastic bag"], # class of labels
-                max_samples=1000, # max number of samples to download
+                max_samples=samples, # max number of samples to download
         )
