f8eb560369c214f02f4a4a0585a49383c788264c
Akshith Kandivanam <akshithkandivanam@gmail.com>
2023-10-19 20:42:22 -0400

    Implemented lambda func to get batch transformation job status

---
+++ aws-lambda-funcs/batch-transform-job-status-func.py
@@ -0,0 +1,23 @@
+import json
+import boto3
+from datetime import datetime
+
+# set-up the sagemaker client.
+client = boto3.client('sagemaker') 
+
+# get the date for the job.
+current_datetime = datetime.today()
+formatted_date = current_datetime.strftime('%Y-%m-%d')
+formatted_time = current_datetime.strftime('%H-%M-%S')
+    
+def lambda_handler(event, context):
+    try:
+        # find and return the status of the batch transformation job.
+        response = client.describe_transform_job(TransformJobName = f'{formatted_date}-{formatted_time}-object-detection-unique')
+        return response['TransformJobStatus']
+    
+    # catch any errors.
+    except Exception as e:
+        print(e)
+        message = 'Error getting Batch  Transformation Job status'
+        raise Exception(message)
